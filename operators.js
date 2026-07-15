8b8343445392bad3e5330f7b95ef539123013df0
Manisha1997-25 <veermanisha1997@gmail.com>
2026-07-15 11:17:16 +0530

    Operators

---
+++ operators.js
@@ -0,0 +1,105 @@
+// Perform Arithematic Operations
+
+let a=10;
+let b=20;
+
+let c=a+b;
+let d=a-b;
+let e=a*b;
+let f=a/b;
+let g=a%b;
+
+console.log("Addition = " +c);
+console.log("Addition = " +d);
+console.log("Addition = " +e);
+console.log("Addition = " +f);
+console.log("Addition = " +g);
+
+
+// Perform Comparison operators
+let aa=10;
+let bb="10";
+let cc='10';
+let dd=20;
+
+console.log("is equal with bb = " + (aa==bb));
+console.log("is equal with cc = " + (aa==cc));
+console.log("is equal with dd = " + (aa==dd));
+console.log("is triple equal with bb = " + (aa===bb));
+console.log("is triple equal with cc = " + (aa===cc));
+console.log("is triple equal with dd = " + (aa===dd));
+console.log("is less with bb = " + (aa<bb));
+console.log("is less with cc = " + (aa<cc));
+console.log("is less with dd = " + (aa<dd));
+console.log("is less than equal to with bb = " + (aa<=bb));
+console.log("is less than equal to with cc = " + (aa<=bb));
+console.log("is less than equal to with dd = " + (aa<=dd));
+console.log("is greater with bb = " + (aa>bb));
+console.log("is greater with cc = " + (aa>cc));
+console.log("is greater with dd = " + (aa>dd));
+console.log("is greater than equal to with bb = " + (aa>=bb));
+console.log("is greater than equal to with cc = " + (aa>=bb));
+console.log("is greater than equal to with dd = " + (aa>=dd));
+console.log("is not equal to with bb = " + (aa!=bb));
+console.log("is not equal to with cc = " + (aa!=cc));
+console.log("is not equal to with dd = " + (aa!=dd));
+console.log("is not double equal to with bb = " + (aa!==bb));
+console.log("is not double equal to with cc = " + (aa!==cc));
+console.log("is not double equal to with dd = " + (aa!==dd));
+
+//Performing Logical Operator
+
+let age = 21;  //&& both condition true the op will get true
+let hasVoterId = true;
+
+if(age >= 18 && hasVoterId){
+    console.log( "Eligible");
+}
+else{
+    console.log("not eligible");
+    
+}
+
+//|| if at least one condition true the op will get true
+let isWeekend = false;
+let isHoliday = true;
+
+console.log(isWeekend || isHoliday);
+
+//! reverse the boolean value 
+
+let isLoggedIn = true;
+console.log(!isLoggedIn);
+
+//Perform Assignment Operators
+
+let x=10;
+console.log(x);  //10
+
+x+=5;  //x+5=
+console.log(x);  //15
+
+x-=5;  //x-5=
+console.log(x);  //10
+
+x*=5;  //x*5=
+console.log(x);  //50
+
+x/=5;  //x/5=
+console.log(x);  //10
+
+x%=5;  //x%5=
+console.log(x);  //0
+
+x**=5  //x**5=
+console.log(x);  //0
+
+
+
+
+
+
+
+
+
+
