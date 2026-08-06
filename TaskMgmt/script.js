bf2229f4145f5ebaa70ea3ba8b9af1a7bbfab987
Manisha1997-25 <veermanisha1997@gmail.com>
2026-08-06 14:45:54 +0530

    Task Management

---
+++ TaskMgmt/script.js
@@ -0,0 +1,83 @@
+const tasks = [
+{
+    id:101,
+    title:"study basic of Programming",
+    description:"Watch shorts or reels on Programming",
+    status:"Pending"
+},
+{
+    id:101,
+    title:"study Advanced css",
+    description:"For interview Preparations",
+    status:"Completed"
+}]
+
+const titleElmt = document.getElementById("taskTitleInput")
+const descriptionElmt = document.getElementById("taskDescriptionInput")
+function renderTasks(){
+    document.getElementById("renderTasks").innerHTML = tasks.map((tsk, i)=>`
+                            <tr>
+                                <td>${i + 1}</td>
+                                <td>${tsk.title}</td>
+                                <td>${tsk.description}</td>
+                                <td>${tsk.status}</td>
+                                <td>
+                                   <button class="btn btn-success" title="Edit task" 
+                                   onclick="EditTask(${tsk.id})">
+                                   <i class="fa-solid fa-pen-to-square"></i>
+                                   </button>
+                                   <button class="btn btn-danger" title="Delete task" 
+                                   onclick="deleteTask(${tsk.id})">
+                                   <i class="fa-solid fa-trash-can"></i>
+                                   </button> 
+                                </td>
+                            </tr>
+    `).join('')
+}
+
+function addNewTask(){
+    titleInput =titleElmt.value
+    descriptionInput = descriptionElmt.value
+
+    const newTask = {
+        id:Date.now(),  //return in milisecond
+        title:titleInput,
+        description:descriptionInput,
+        status:"Pending"
+
+    }
+    tasks.push(newTask)
+    renderTasks()
+
+    titleElmt.value = ''
+    descriptionElmt.value =''
+}
+
+function deleteTask(ID){
+    index = tasks.findIndex((t)=> t.id ==ID)
+    if(index == -1){
+        alert("Task Not Found")
+    }else{
+        tasks.splice(index, 1)
+    }
+    renderTasks()
+}
+
+function EditTask(ID){
+    index = tasks.findIndex((t)=> t.id ==ID)
+    if(index == -1){
+        alert("Task Not Found")
+    }else{
+        if(tasks[index].status == "Pending"){
+            tasks[index].status = "completed"
+        }else{
+            tasks[index].status = "Pending"
+        }
+    }
+    renderTasks()
+}
+
+window.addEventListener('load', () =>{
+    renderTasks()
+})
+    
